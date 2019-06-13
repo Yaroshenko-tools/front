aa2fae6177b9025313f236a1eda230e598c67f98
alyar88 <lsd00000>
2019-06-13 17:47:24 +0300

    statistical calc update

--- src/components/StatisticalSignificance.vue
+++ src/components/StatisticalSignificance.vue
@@ -1,29 +1,54 @@
 <template>
 	<div>
 		<h1 class="title mb-3">Калькулятор статистической значимости</h1>
-		<p class="mb-0">Введите число кликов и конверсий (или показов и кликов для CTR) и получите интервалы, в которых могут лежать эти значения с вероятностью 80, 90, 95 и 99%</p>
+		<p class="mb-0">Введите число кликов и конверсий (или коэффициент конверсии) и получите интервалы, в которых может лежать конверсия с вероятностью 70%, 80%, 90%, 95% и 99%</p>
 		<v-layout row>
-			<v-flex class="" xs4 sm4 md4>
-				<v-text-field v-model="nCount" type="number" label="Число кликов (или показов, для CTR)"></v-text-field>
-				<v-text-field v-model="convCount" type="number" label="Число конверсий (или кликов, для CTR)"></v-text-field>
-				<v-text-field v-model="convCoef" suffix="%" type="number" label="Коэфициент конверсии в % (или CTR)"></v-text-field>
+			<v-flex class="" xs5 sm5 md5>
+				<v-text-field v-model="nCount" type="number" label="Число кликов"></v-text-field>
+				<v-layout row>
+					<v-flex>
+						<v-text-field v-model="convCount" type="number" label="Число конверсий" v-on:change="updateConvCoef()"></v-text-field>
+					</v-flex>
+					<v-flex class="mt-4">или</v-flex>
+					<v-flex>
+						<v-text-field v-model="convCoef" suffix="%" type="number" label="Коэфициент конверсии" v-on:change="updateConvCount()"></v-text-field>
+					</v-flex>
+				</v-layout>
 			</v-flex>
-			<v-flex class="" xs8 sm8 md8>
+			<v-flex class="" xs7 sm7 md7>
 				<v-card>
 					<v-card-text>
 						<h3>С вероятностью</h3>
 						<ul>
+							<li>70% &mdash; конверсия находится между <strong>{{interval70[0]}}%</strong> и <strong>{{interval70[1]}}%</strong></li>
 							<li>80% &mdash; конверсия находится между <strong>{{interval80[0]}}%</strong> и <strong>{{interval80[1]}}%</strong></li>
-							<!--<li>90% &mdash; конверсия находится между <strong>{{interval90[0]}}%</strong> и <strong>{{interval90[1]}}%</strong></li>-->
+							<li>90% &mdash; конверсия находится между <strong>{{interval90[0]}}%</strong> и <strong>{{interval90[1]}}%</strong></li>
 							<li>95% &mdash; конверсия находится между <strong>{{interval95[0]}}%</strong> и <strong>{{interval95[1]}}%</strong></li>
 							<li>99% &mdash; конверсия находится между <strong>{{interval99[0]}}%</strong> и <strong>{{interval99[1]}}%</strong></li>
 						</ul>
+
+						<!--<v-progress-circular-->
+						<!--:rotate="360"-->
+						<!--:size="255"-->
+						<!--:width="3"-->
+						<!--:value="70"-->
+						<!--color="red"-->
+						<!--&gt;<strong>{{interval70[0]}}%</strong> < Cv < <strong>{{interval70[1]}}%</strong>-->
+						<!--</v-progress-circular>-->
 					</v-card-text>
 				</v-card>
 			</v-flex>
 
 		</v-layout>
-		
+		<v-layout>
+			<v-flex class="mt-5">
+				<p>
+					На самом деле, это не совсем калькулятор статистической занчимости, ибо сама статистическая занчимость здесь явно не фигурирует.
+					<br/>
+					В математической модели используются некоторые допущения, поэтому значения - не истина в последней инстанции. Имейте ввиду :)
+				</p>
+			</v-flex>
+		</v-layout>
 	</div>
 </template>
 
@@ -39,18 +64,20 @@
 
 		}),
 		computed: {
+			interval70() {
+				return getInterval(this.nCount, this.convCount / this.nCount, 1.036)
+			},
 			interval80() {
-				return getInterval(this.nCount, this.convCoef / 100, 1.282)
-
+				return getInterval(this.nCount, this.convCount / this.nCount, 1.282)
 			},
 			interval90() {
-				return getInterval(this.nCount, this.convCoef / 100, 1.645)
+				return getInterval(this.nCount, this.convCount / this.nCount, 1.645)
 			},
 			interval95() {
-				return getInterval(this.nCount, this.convCoef / 100, 1.96)
+				return getInterval(this.nCount, this.convCount / this.nCount, 1.96)
 			},
 			interval99() {
-				return getInterval(this.nCount, this.convCoef / 100, 2.57)
+				return getInterval(this.nCount, this.convCount / this.nCount, 2.57)
 			}
 		},
 		created() {
@@ -62,16 +89,15 @@
 		updated() {
 			localStorage.setItem('stats-calc', JSON.stringify(this._data))
 		},
-		methods: {},
-		watch: {
-			convCount(value) {
-				this.convCoef = ((value / this.nCount) * 100).toFixed(2)
+		methods: {
+			updateConvCoef() {
+				this.convCoef = (this.convCount / this.nCount * 100).toFixed(2).replace(/([0-9]+(\.[0-9]+[1-9])?)(\.?0+$)/, '$1')
 			},
-			convCoef(value) {
-				this.convCount = (this.nCount * (value / 100)).toFixed(0)
+			updateConvCount() {
+				this.convCount = (this.convCoef / 100 * this.nCount).toFixed(0)
 			}
-
 		},
+		watch: {},
 	}
 
 	function getInterval(n, p, nSigma) {
@@ -79,7 +105,10 @@
 		let mean = n * p;
 		let minConv = (mean - std * nSigma) / n;
 		let maxConv = (mean + std * nSigma) / n;
-		return [(minConv * 100).toFixed(2), (maxConv * 100).toFixed(2)]
+		if (minConv < 0) {
+			minConv = 0
+		}
+		return [(minConv * 100).toFixed(2).replace(/([0-9]+(\.[0-9]+[1-9])?)(\.?0+$)/, '$1'), (maxConv * 100).toFixed(2).replace(/([0-9]+(\.[0-9]+[1-9])?)(\.?0+$)/, '$1')]
 	}
 </script>
 
