30b0d863976f5d1e417df394220d832d6e0811a9
Romuald Shmidtelson <d-w-b@mail.ru>
2022-05-15 01:32:34 +0300

    Added English seo titles

--- locales/en.js
+++ locales/en.js
@@ -1,3 +1,22 @@
 export default {
+  index_seo_title: 'Utilities for working with contextual advertising from Alexey Yaroshenko',
+  index_seo_description: 'Free utilities and services that speed up the launch of an advertising campaign in Yandex.Direct and Google Adwords by 2-10 times!',
 
+  utm_seo_title: 'UTM tag generator for AdWords, Direct, VK, Facebook, Instagram',
+  utm_seo_description: 'Create a link with advanced UTM tags for Yandex Direct, Google Adwords, VK, Facebook or Instagram in 15 seconds. The utility will help!',
+
+  matchtypes_seo_title: 'Google Ads match types tool',
+  matchtypes_seo_description: 'The tool converts keywords to any of the 3 Google AdWords match types: broad match, phrase match and exact match.',
+
+  google_ads_generator_seo_title: 'Google Ads Generator',
+  google_ads_generator_seo_description: 'Generate Google Ads Campaigns, Ad Sets, Keys in Different Match Types and Ads in 60 Seconds!',
+
+  stats_calc_seo_title: 'Statistical Significance Calculator',
+  stats_calc_seo_description: 'Enter the number of clicks and conversions (or conversion rate) and get the intervals in which a conversion can lie with a probability of 70%, 80%, 90%, 95% and 99%',
+
+  matchtypes_direct_seo_title: 'Utility for adding Yandex.Direct operators to keywords',
+  matchtypes_direct_seo_description: 'The utility adds operators !exclamation !mark, "quotation marks" and [right-angle brackets], fixing the word form, number of words and their order to keywords for Yandex.Direct.',
+
+  not_found_seo_title: 'Page not found (404)',
+  not_found_seo_description: 'Go back to home page and try again'
 }
